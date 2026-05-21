7bd734ef9113188721e0a2c5af9db3a858d0860c
benny <lealbenedictavi1117@gmail.com>
2026-05-21 12:54:03 +0800

    changes
    components to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
